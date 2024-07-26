8bb51986ccca5ece06a72bf6ae42f25810f1b94e
Viktoriia Nykyforova <113258083+ViktoriiaKN@users.noreply.github.com>
2024-07-26 21:29:57 +0200

    adjusted main pages routes

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,16 @@ import { Route, Routes } from "react-router-dom"
 import MoviesPage from "./pages/MoviesPage/MoviesPage"
 import HomePage from "./pages/HomePage/HomePage"
 import NotFoundPage from "./pages/NotFoundPage/NotFoundPage"
+import Header from "./components/Header/Header"
 
 
 function App() {
 return (
   <div>
+    <Header/>
     <Routes>
       <Route path="/" element={<HomePage/>}/>
-      <Route path="/movie" element={<MoviesPage/>}/>
+      <Route path="/movies" element={<MoviesPage/>}/>
       <Route path="*" element={<NotFoundPage/>}/>
     </Routes>
   </div>
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,26 @@
+import clsx from "clsx";
+import { NavLink } from "react-router-dom"
+import s from './Header.module.css';
+
+const Header = () => {
+
+  const buildLinkClass = ({ isActive }) => {
+    return clsx(s.link, isActive && s.active);
+  };
+
+  return (
+    <header className={s.header}>
+      <div>
+      <nav>
+<ul className={s.nav}>
+  <li><NavLink className={buildLinkClass} to='/'>Home</NavLink></li>
+  <li><NavLink className={buildLinkClass} to='/movies'>Movies</NavLink></li>
+</ul>
+</nav>
+      </div>
+    </header>
+    
+  )
+}
+
+export default Header
