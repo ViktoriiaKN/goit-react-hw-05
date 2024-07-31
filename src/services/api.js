08761954680b5d31c02da77291b96998950235db
Viktoriia Nykyforova <113258083+ViktoriiaKN@users.noreply.github.com>
2024-07-31 19:56:25 +0200

    A separate file for requests

--- src/services/api.js
+++ src/services/api.js
@@ -1,34 +1,63 @@
-import axios from "axios";
+import axios from 'axios';
 
-const url = 'https://api.themoviedb.org/3/search/movie?include_adult=false&language=en-US&page=1';
+const API_TOKEN =
+  'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2MmRhMDNlYmNlNzU4YTdjNDk1ZmQ1YWY2ODBmN2EwZiIsIm5iZiI6MTcyMjA3MzgxOC4zMTE2MjYsInN1YiI6IjY2YTNhZmNjN2NkOWIyM2Y2NjgxMTQzYSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.bQPLjzaf_sLDf46ewFKmoNXn80t459nMd7iH94Kl3hQ';
 
-export const fetcImages = async (query, perPage = 5, page = 1) => {
+const instance = axios.create({
+  baseURL: 'https://api.themoviedb.org/3',
+  headers: {
+    Authorization: `Bearer ${API_TOKEN}`,
+  },
+});
+
+export const fetchTrendingMovies = async () => {
   try {
-    const response = await axios.get(`${url}search/movies`, {
-      params: {
-        query,
-        per_page: perPage,
-        page,
-      },
-      headers: {
-        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2MmRhMDNlYmNlNzU4YTdjNDk1ZmQ1YWY2ODBmN2EwZiIsIm5iZiI6MTcyMjA3MzgxOC4zMTE2MjYsInN1YiI6IjY2YTNhZmNjN2NkOWIyM2Y2NjgxMTQzYSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.bQPLjzaf_sLDf46ewFKmoNXn80t459nMd7iH94Kl3hQ',
-      },
-    });
-    return response.data;
+    const response = await instance.get('/trending/movie/day');
+    return response.data.results;
   } catch (error) {
-    console.error('Error fetching movies from TMDB:', error);
+    console.error('Error fetching trending movies:', error);
     throw error;
   }
 };
 
-
-const options = {
-  headers: {
-	// Замість api_read_access_token вставте свій токен
-    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2MmRhMDNlYmNlNzU4YTdjNDk1ZmQ1YWY2ODBmN2EwZiIsIm5iZiI6MTcyMjA3MzgxOC4zMTE2MjYsInN1YiI6IjY2YTNhZmNjN2NkOWIyM2Y2NjgxMTQzYSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.bQPLjzaf_sLDf46ewFKmoNXn80t459nMd7iH94Kl3hQ'
+export const searchMovies = async (query) => {
+  try {
+    const response = await instance.get('/search/movie', {
+      params: { query },
+    });
+    return response.data.results;
+  } catch (error) {
+    console.error('Error searching movies:', error);
+    throw error;
   }
 };
 
-axios.get(url, options)
-  .then(response => console.log(response))
-  .catch(err => console.error(err));
+export const fetchMovieDetails = async (movieId) => {
+  try {
+    const response = await instance.get(`/movie/${movieId}`);
+    return response.data;
+  } catch (error) {
+    console.error('Error fetching movie details:', error);
+    throw error;
+  }
+};
+
+export const fetchMovieCredits = async (movieId) => {
+  try {
+    const response = await instance.get(`/movie/${movieId}/credits`);
+    return response.data.cast;
+  } catch (error) {
+    console.error('Error fetching movie credits:', error);
+    throw error;
+  }
+};
+
+export const fetchMovieReviews = async (movieId) => {
+  try {
+    const response = await instance.get(`/movie/${movieId}/reviews`);
+    return response.data.results;
+  } catch (error) {
+    console.error('Error fetching movie reviews:', error);
+    throw error;
+  }
+};
