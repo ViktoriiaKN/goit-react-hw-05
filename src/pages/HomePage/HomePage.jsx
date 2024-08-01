da54ce2f1acde5eb44250d55dfca89d9646e2f78
Viktoriia Nykyforova <113258083+ViktoriiaKN@users.noreply.github.com>
2024-08-01 14:20:25 +0200

    HomePage logic

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,9 +1,31 @@
+import { useEffect, useState } from "react"
+import { fetchTrendingMovies } from "../../services/api";
+import MovieList from "../../components/MovieList/MovieList";
 
 const HomePage = () => {
+const [movies, setMovies] = useState([]);
+const [error, setError] = useState(null);
+
+useEffect (() => {
+  const getTrendingMovies = async () => {
+    try {
+      const movies = await fetchTrendingMovies();
+      setMovies(movies)
+    } catch (error) {
+      setError('Failed to fetch trending movies');
+    }
+  }
+  getTrendingMovies();
+}, [] )
+
   return (
-    <div>HomePage</div>
+    <div>
+      <h1>Trending Movies</h1>
+      {error && <p>{error}</p>}
+      {movies.length > 0 && <MovieList movies={movies}/>}
+    </div>
   )
 }
 
-export default HomePage
+export default HomePage;
 
